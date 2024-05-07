ce94efa5562a4efefbfeeaad60315c2e0b9fc5c9
wj461 <wj4wj461@gmail.com>
2024-05-07 12:51:01 +0800

    three use test globel and all_d

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-data = pd.read_csv('./data/housing_data.csv') 
-test = pd.read_csv('./data/test.csv')
+data_o = pd.read_csv('./data/housing_data.csv')
+test_o = pd.read_csv('./data/test.csv')
 
 def function_2(p_x, x):
     return p_x * x[0]
@@ -38,21 +38,20 @@ def two_d_linear_regression():
     plt.show() 
 
 def function_3(p_m, x):
-        # return p_x * x[0] + p_y * x[1]
         a = np.dot(p_m, x)
-        print (f'f3:{p_m}\n{a}')
         return a
 
-def three_d_linear_regression():
-    data = pd.read_csv('./data/3d.csv')
-    test = pd.read_csv('./data/test_3d.csv')
+def three_d_linear_regression(need):
+    data = data_o[need]
+    # test = pd.read_csv('./data/test_3d.csv')
+    test = test_o[need[: -1]]
 
     fig = plt.figure()
     ax1 = plt.axes(projection='3d')
-    ax1.scatter(data['CRIM'], data['AGE'], data['MEDV'])
-    ax1.set_xlabel('CRIM')
-    ax1.set_ylabel('AGE')
-    ax1.set_zlabel('MEDV')
+    ax1.scatter(data[need[0]], data[need[1]], data[need[2]])
+    ax1.set_xlabel(need[0])
+    ax1.set_ylabel(need[1])
+    ax1.set_zlabel(need[2])
 
     data = data.values
     test = test.values
@@ -70,23 +69,33 @@ def three_d_linear_regression():
             p_m = np.vstack([p_m, [i, j]])
 
     p_z = function_3(p_m, x)
-    ax1.plot(p_m[:,0], p_m[:,1], p_z)
+    ax1.plot(p_m[:,0], p_m[:,1], p_z, c='r', alpha=0.5)
 
     ans = np.dot(test, x)
+    print("ans : ", ans)
 
-    # print(ans)
     ax1.scatter(test[:, 0], test[:, 1], ans, c='r')
-    # ax1.plot_surface(p_mx, p_my, ans, c='r')
 
     plt.show() 
 
 
-three_d_linear_regression()
+def all_D():
+    data = data_o.values
+    test = test_o.values
+
+    A = data[:, :-1]
+    b = data[:, -1].T
+
+    Q, R = np.linalg.qr(A)
+
+    x = np.linalg.solve(R, np.dot(Q.T, b))
+    print(np.dot(test, x))
+
 # two_d_linear_regression()
 
-# need = ['CRIM', 'ZN', 'INDUS', 'CHAS']
-# split_data = data[need]
-# print(split_data)
+need = ['CRIM', 'AGE', 'MEDV']
+three_d_linear_regression(need)
+all_D()
 
 
 # test data
